5c5e024f749d938fe3a1a5ed56d99d18f20d5369
mike <mjowiti@gmail.com>
2023-06-13 11:12:51 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+
+
+    let multiply = num1 * num2;
+     console.log(multiply)
+
+   let random =Math.ceil(Math.random()*10)
+   console.log(random)
+
+   
+    
+
+   let max=Math.max(1, 2, 3, 4, 20);
+   console.log(max)
+   
+
+
+  const num3=24;
+  const num4=5;
+const mod= num3%num4;
+console.log(mod)
